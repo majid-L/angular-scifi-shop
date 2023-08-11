767260c485882d973f3da5bf547ad4fa98fcfa4c
3782291211 <Laklouk98@hotmail.com>
2023-08-11 23:45:35 +0100

    Adding auth form component

---
+++ src/app/auth/auth-form/auth-form.component.ts
@@ -0,0 +1,68 @@
+import { Component, Input } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { loginRequest } from 'src/app/ngrx/auth/auth.actions';
+
+@Component({
+  selector: 'app-auth-form',
+  templateUrl: './auth-form.component.html',
+  styleUrls: ['./auth-form.component.sass']
+})
+export class AuthFormComponent {
+  @Input() formType = '';
+  formField = ['', [Validators.required, Validators.pattern('^[^ ]+$')]];
+  hide = true;
+  formFields = {
+    name: [''],
+    email: [''],
+    username: 'alexnes',
+    password: 'password'
+   // username: this.formField,
+   // password: this.formField
+  };
+  authForm = this.formBuilder.group(this.formFields);
+  status$: Observable<Status> = this.store.select(state => state.authSlice.status);
+
+  constructor(private store: Store<AppState>, private formBuilder: FormBuilder) {}
+
+  showErrorMessage(input: "name" | "email" | "username" | "password") {
+    if (this.authForm.controls[input].hasError('required')) {
+      return 'You must enter a value';
+    }
+    if (this.authForm.controls[input].hasError('pattern')) {
+      return 'Field must not contain blank spaces.';
+    }
+    if (this.authForm.controls[input].hasError('email')) {
+      return 'Email format needs to be 123@abc.xyz';
+    }
+    return;
+  }
+
+  ngOnChanges({ formType: { currentValue } }: 
+    { formType: { currentValue: string } }) {
+    if (currentValue === 'Signup') {
+      this.authForm = this.formBuilder.group({
+        ...this.formFields,
+        name: this.formField,
+        email: ['', [Validators.required, Validators.email]]
+      });
+    } else if (currentValue === 'Login') {
+      this.authForm = this.formBuilder.group(this.formFields);
+    }
+  }
+
+  togglePasswordField(e: Event) {
+    e.preventDefault();
+    this.hide = !this.hide;
+  }
+
+  onSubmit() {
+    const formValue = this.authForm.value as AuthCredentials;
+    if (formValue.name && formValue.email) {
+      //this.
+    } else {
+      this.store.dispatch(loginRequest(formValue));
+    }
+  }
+}
