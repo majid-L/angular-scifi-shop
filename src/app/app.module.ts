fa955704925344a43e1ef5e55797ce00db05f221
3782291211 <Laklouk98@hotmail.com>
2023-08-13 23:26:10 +0100

    Adding imports to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
+import { reducers, metaReducers } from './ngrx';
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material/material.module';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { CookieService } from 'ngx-cookie-service';
+import { AccountModule } from './account/account.module';
+import { DialogComponent } from './dialog/dialog.component';
+import { CartModule } from './cart/cart.module';
+import { CartSidebarComponent } from './cart/cart-sidebar/cart-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    CartSidebarComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AuthModule,
+    ProductsModule,
+    AccountModule,
+    CartModule,
+    MaterialModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    EffectsModule.forRoot([])
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
