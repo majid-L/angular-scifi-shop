06d0a2815f4fed1b6c768dd77adf78f6dccbc373
3782291211 <Laklouk98@hotmail.com>
2023-08-13 23:27:05 +0100

    Dispatching actions from products component

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { selectProducts, selectStatus } from 'src/app/ngrx';
+import { Observable, Subscription } from 'rxjs';
+import { selectCartItems, selectCartStatus, selectProducts, selectProductsStatus } from 'src/app/ngrx';
+import { addToCart } from 'src/app/ngrx/cart/cart.actions';
 import { loadProducts } from 'src/app/ngrx/products/products.actions';
 
 @Component({
@@ -11,11 +12,26 @@ import { loadProducts } from 'src/app/ngrx/products/products.actions';
 })
 export class ProductListComponent implements OnInit {
   products$: Observable<Product[] | null> = this.store.select(selectProducts);
-  status$: Observable<Status> = this.store.select(selectStatus);
+  productStatus$: Observable<Status> = this.store.select(selectProductsStatus);
+  cartStatus$: Observable<Status> = this.store.select(selectCartStatus);
+  cart$: Observable<CartItems | null> = this.store.select(selectCartItems);
+  loggedInUserId: string | null = window.localStorage.getItem('userId');
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit() {
 	  this.store.dispatch(loadProducts());
+  }
+
+  addToCart(e: MouseEvent, productId: number) {
+    this.store.dispatch(addToCart({ 
+      productId,
+      customerId: Number(this.loggedInUserId),
+      quantity: 1
+     }));
+  }
+
+  cartIncludesItem(cartItems: CartItem[] | null, productId: number) {
+    return cartItems && cartItems.find(item => item.productId === productId);
   }
 }
