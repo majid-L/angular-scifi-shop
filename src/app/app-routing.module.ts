9c37507b559fbe9be8ef71c9b47f519798be54c2
3782291211 <Laklouk98@hotmail.com>
2023-08-21 23:23:40 +0100

    Adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account/account.component';
+import { CheckoutComponent } from './checkout/checkout/checkout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent},
+  { path: "products", component: ProductListComponent },
+  { path: "account", component:  AccountComponent},
+  { path: "checkout", component: CheckoutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
