11b1687cb3a1dbced87631942e54259add36625e
3782291211 <Laklouk98@hotmail.com>
2023-08-25 20:16:23 +0100

    Updating dialog component

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,8 +1,10 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { hideDialog } from '../ngrx/notification/notification.actions';
+import { selectData } from '../ngrx/notification/notification.feature';
 
 @Component({
   selector: 'app-dialog',
@@ -10,9 +12,29 @@ import { hideDialog } from '../ngrx/notification/notification.actions';
   styleUrls: ['./dialog.component.sass']
 })
 export class DialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: Observable<DialogContent>, private store: Store<AppState>) {}
+  readonly errorStatus$: Observable<DialogContent | null> = 
+    this._store.select(selectData);
+  private _subscription = Subscription.EMPTY;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: Observable<DialogContent>, 
+    private _store: Store<AppState>,
+    private _router: Router
+  ) {}
+
+  ngOnInit() {
+    this._subscription = this.errorStatus$.subscribe(data => {
+      if (data?.error?.status === 404) {
+        this._router.navigate(['/']);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this._subscription.unsubscribe();
+  }
 
   hideOverlay() {
-    this.store.dispatch(hideDialog());
+    this._store.dispatch(hideDialog());
   }
 }
