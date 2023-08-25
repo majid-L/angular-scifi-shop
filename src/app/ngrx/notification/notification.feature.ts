0f017d28b7cc8dc54889794e08cd8aaa84b425a3
3782291211 <Laklouk98@hotmail.com>
2023-08-25 20:17:03 +0100

    Creating NGRX feature for notification states

---
+++ src/app/ngrx/notification/notification.feature.ts
@@ -0,0 +1,42 @@
+import { createFeature, createReducer, on } from '@ngrx/store';
+import { showDialog, hideDialog, httpError, notify } from "./notification.actions";
+
+export const initialState: NotificationState = {
+  showDialog: false,
+  data: null
+};
+
+export const notificationReducer = createReducer(
+  initialState,
+  on(showDialog, state => ({...state, showDialog: true})),
+  on(hideDialog, state => ({...state, showDialog: false})),
+  on(httpError, (state, { error }) => {
+    return {
+      showDialog: true,
+      data: {
+        title: "There was an error.",
+        error
+      }
+    }
+  }),
+  on(notify, (state, { title, content, buttons }) => {
+    return {
+      showDialog: true,
+      data: {
+        title,
+        content,
+        buttons
+      }
+    }
+  })
+);
+
+export const notificationFeature = createFeature({
+  name: "notification",
+  reducer: notificationReducer
+});
+
+export const {
+  selectShowDialog,
+  selectData
+} = notificationFeature;
