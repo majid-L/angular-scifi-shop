afa1ce9d5b634108f302939c0800f43279eaef3b
3782291211 <Laklouk98@hotmail.com>
2023-08-26 15:57:41 +0100

    Adding auth feature to NGRX store

---
+++ src/app/ngrx/auth/auth.feature.ts
@@ -0,0 +1,102 @@
+import { createFeature, createReducer, createSelector, on } from '@ngrx/store';
+import { httpError } from '../notification/notification.actions';
+import { 
+  showAuthOverlay, 
+  hideAuthOverlay, 
+  signup, 
+  loginRequest, 
+  logoutRequest,
+  logoutSuccess, 
+  loginSuccess,
+  resetStatus
+} from './auth.actions';
+
+const initialState: AuthState = {
+  showOverlay: false,
+  currentUser: null,
+  loggedInUserId: window.localStorage.getItem('userId'),
+  loginStatus: "pending",
+  logoutStatus: "pending",
+  signupStatus: "pending",
+  logoutMsg: null
+};
+
+export const authReducer = createReducer(
+  initialState,
+  on(showAuthOverlay, state => ({...state, showOverlay: true})),
+  on(hideAuthOverlay, state => ({...state, showOverlay: false})),
+  on(loginRequest, state => ({ ...state, loginStatus: "loading" as const })),
+  on(loginSuccess, (state, payload) => {
+    return { 
+      ...state, 
+      loggedInUserId: payload.customer.id,
+      currentUser: payload.customer,
+      loginStatus: "success" as const
+     };
+  }),
+  on(logoutRequest, state => ({ ...state, logoutStatus: "loading" as const })),
+  on(logoutSuccess, (state, payload) => {
+    return {
+      ...state,
+      loggedInUserId: null,
+      logoutMsg: payload.msg,
+      logoutStatus: "success" as const
+    };
+  }),
+  on(resetStatus, state => ({ 
+    ...state, 
+    loginStatus: "pending" as const, 
+    logoutStatus: "pending" as const,
+    signupStatus: "pending" as const
+  })),
+  on(httpError, state => ({ 
+    ...state, 
+    loginStatus: "error" as const, 
+    logoutStatus: "error" as const,
+    signupStatus: "error" as const
+  }))
+);
+
+export const authFeature = createFeature({
+  name: "auth",
+  reducer: authReducer,
+  extraSelectors: (state) => ({
+    selectAnyLoadingState: createSelector(
+      state.selectLoginStatus,
+      state.selectLogoutStatus,
+      state.selectSignupStatus,
+      (loginStatus, logoutStatus, signupStatus) => {
+        return loginStatus === "loading"
+          || logoutStatus === "loading"
+          || signupStatus === "loading";
+      }
+    ),
+    selectAuthIsLoading: createSelector(
+      state.selectLoginStatus,
+      state.selectSignupStatus,
+      (loginStatus, signupStatus) => {
+        return loginStatus === "loading" || signupStatus === "loading";
+      }
+    ),
+    selectAuthIsSuccess: createSelector(
+      state.selectLoginStatus,
+      state.selectSignupStatus,
+      (loginStatus, signupStatus) => {
+        return loginStatus === "success" || signupStatus === "success";
+      }
+    ),
+  })
+});
+
+export const {
+  selectCurrentUser,
+  selectLoggedInUserId,
+  selectShowOverlay,
+  selectLoginStatus,
+  selectLogoutStatus,
+  selectSignupStatus,
+  selectAnyLoadingState,
+  selectAuthIsLoading,
+  selectAuthIsSuccess,
+  selectLogoutMsg
+} = authFeature;
