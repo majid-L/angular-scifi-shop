2dca264914cce0f3aa7646e434144b7a579a061d
3782291211 <Laklouk98@hotmail.com>
2023-08-27 23:37:05 +0100

    Adding actions for reviews slice

---
+++ src/app/ngrx/reviews/reviews.actions.ts
@@ -0,0 +1,49 @@
+import { createAction, props } from "@ngrx/store";
+
+export const loadProductReviews = createAction(
+  "[Reviews Component] Load Product Reviews",
+  props<{ productId: number }>()
+);
+export const loadProductReviewsSuccess = createAction(
+  "[Reviews Component] Load Product Reviews - Success",
+  props<ReviewsResponse>()
+);
+
+export const loadCustomerReviews = createAction(
+  "[Reviews Component] Load Customer Reviews",
+  props<{ customerId: number }>()
+);
+export const loadCustomerReviewsSuccess = createAction(
+  "[Reviews Component] Load Customer Reviews - Success",
+  props<ReviewsResponse>()
+);
+
+export const createReview = createAction(
+  "[Reviews Component] Create Review - Loading",
+  props<NewReviewRequest>()
+);
+export const createReviewSuccess = createAction(
+  "[Reviews Component] Create Review - Success",
+  props<{ newReview: Review }>()
+);
+
+export const updateReview = createAction(
+  "[Reviews Component] Update Review - Loading",
+  props<{ 
+    reviewId: number, 
+    requestBody: UpdateReviewRequest 
+  }>()
+);
+export const updateReviewSuccess = createAction(
+  "[Reviews Component] Update Review - Success",
+  props<{ updatedReview: Review }>()
+);
+
+export const deleteReview = createAction(
+  "[Reviews Component] Delete Review - Loading",
+  props<{ reviewId: number }>()
+);
+export const deleteReviewSuccess = createAction(
+  "[Reviews Component] Delete Review - Success",
+  props<{ deletedReview: Review }>()
+);
