5f649f90e767be8acaeccf3cdf9bab979f06a4f5
3782291211 <Laklouk98@hotmail.com>
2023-09-08 15:00:06 +0100

    Updating product list component

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,8 +1,11 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { MatButtonToggleChange } from '@angular/material/button-toggle';
+import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { combineLatest, map, Observable, Subscription } from 'rxjs';
 import { selectActiveId, selectUpdateStatus } from 'src/app/ngrx/cart/cart.feature';
-import { loadProducts } from 'src/app/ngrx/products/products.actions';
+import { selectCategories, selectSuppliers } from 'src/app/ngrx/categories/categories.feature';
 import { selectLoadStatus, selectProducts } from 'src/app/ngrx/products/products.feature';
 
 @Component({
@@ -11,16 +14,81 @@ import { selectLoadStatus, selectProducts } from 'src/app/ngrx/products/products
   styleUrls: ['./product-list.component.sass']
 })
 export class ProductListComponent implements OnInit {
-  products$: Observable<Product[] | null> = this.store.select(selectProducts);
-  productLoadStatus$: Observable<Status> = this.store.select(selectLoadStatus);
-  cartUpdateStatus$: Observable<Status> = this.store.select(selectUpdateStatus);
-  activeId$: Observable<number> = this.store.select(selectActiveId);
+  products$: Observable<Product[] | null> = this._store.select(selectProducts);
+  categories$: Observable<Category[] | null> = this._store.select(selectCategories);
+  suppliers$: Observable<Supplier[] | null> = this._store.select(selectSuppliers);
+  productLoadStatus$: Observable<Status> = this._store.select(selectLoadStatus);
+  cartUpdateStatus$: Observable<Status> = this._store.select(selectUpdateStatus);
+  activeId$: Observable<number> = this._store.select(selectActiveId);
+  private _breakpointSubscription = Subscription.EMPTY;
+  private _streamSubscription = Subscription.EMPTY;
+  listDisplayStyle = "grid";
+  showDisplayToggle = true;
+  category: Category | null | undefined = null;
+  supplier: Supplier | null | undefined = null;
+  datastream$ = combineLatest([
+    this.categories$, this.suppliers$, this._route.queryParamMap
+  ]).pipe(map(([categories, suppliers, queryParamMap]) => ({
+    categories, suppliers, queryParamMap
+  })));
 
   constructor(
-    private store: Store<AppState>
+    private _store: Store<AppState>,
+    private _route: ActivatedRoute,
+    private _breakPointObserver: BreakpointObserver
   ) {}
 
   ngOnInit() {
-	  this.store.dispatch(loadProducts());
+    this._breakpointSubscription = this._breakPointObserver
+      .observe('(max-width: 880px)')
+      .subscribe(({ matches }) => {
+        if (matches) {
+          this.listDisplayStyle = "grid";
+          this.showDisplayToggle = false;
+        } else {
+          this.showDisplayToggle = true;
+        }
+      });
+
+    this._streamSubscription = this.datastream$
+      .subscribe(({ categories, suppliers, queryParamMap }) => {
+        const categoryParam = queryParamMap.get("category");
+        const supplierParam = queryParamMap.get("supplier");
+        if (categoryParam && categories) {
+          this.category = categories.find(category => category.name === categoryParam);
+        } else if (!categoryParam) {
+          this.category = null;
+        }
+
+        if (supplierParam && suppliers) {
+          this.supplier = suppliers.find(supplier => supplier.name === supplierParam);
+        } else if (!supplierParam) {
+          this.supplier = null;
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    this._breakpointSubscription.unsubscribe();
+    this._streamSubscription.unsubscribe();
+  }
+
+  get headerImageSrc() {
+    return this.category ? 
+      `assets/categories/${this.category.thumbnail}.svg` 
+      : this.supplier ? 
+      `assets/suppliers/${this.supplier.thumbnail}.svg` 
+      : "assets/products.svg";
+  }
+
+  get productListClass() {
+    return {
+      "product-list": this.listDisplayStyle === 'list',
+      "product-grid": this.listDisplayStyle === 'grid'
+    };
+  }
+
+  toggleStyle(e: MatButtonToggleChange) {
+    this.listDisplayStyle = e.value;
   }
 }
