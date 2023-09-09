158583d34a315807b4e0e40673f95a4a953cb394
3782291211 <Laklouk98@hotmail.com>
2023-09-09 22:45:56 +0100

    Updating review component

--- src/app/reviews/reviews/reviews.component.ts
+++ src/app/reviews/reviews/reviews.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { selectReviews, selectLoadStatus } from 'src/app/ngrx/reviews/reviews.feature';
+import { selectLoggedInUserId } from 'src/app/ngrx/auth/auth.feature';
+import { deleteReview, resetReviewsStatus, updateActiveId } from 'src/app/ngrx/reviews/reviews.actions';
+import { selectReviews, selectLoadStatus, selectReviewStatus, selectActiveId } from 'src/app/ngrx/reviews/reviews.feature';
+import { ReviewDialogComponent } from '../review-dialog/review-dialog.component';
 
 @Component({
   selector: 'app-reviews',
@@ -9,10 +13,34 @@ import { selectReviews, selectLoadStatus } from 'src/app/ngrx/reviews/reviews.fe
   styleUrls: ['./reviews.component.sass']
 })
 export class ReviewsComponent {
-  readonly reviews$: Observable<Review[] | null> = this._store.select(selectReviews);
-  readonly loadStatus$: Observable<Status> = this._store.select(selectLoadStatus);
+  @Input() showCreateReviewButton = false;
+  @Input() newReviewTemplate: NewReviewRequest & { product: Product } | undefined;
+  readonly reviews$: Observable<Review[] | null> = 
+    this._store.select(selectReviews);
+  readonly loadStatus$: Observable<Status> = 
+    this._store.select(selectLoadStatus);
+  readonly reviewStatus$: Observable<Status> = this._store.select(selectReviewStatus);
+  readonly loggedInUserId$: Observable<string | number | null> = 
+    this._store.select(selectLoggedInUserId);
+  readonly activeId$: Observable<number> = this._store.select(selectActiveId);
 
   constructor(
-    private _store: Store<AppState>
+    private _store: Store<AppState>,
+    public dialog: MatDialog
   ) { }
+
+  showDialog(
+    review: NewReviewRequest | UpdateReviewRequest, 
+    operation: "create" | "update") {
+      this._store.dispatch(resetReviewsStatus());
+      this.dialog.open(ReviewDialogComponent, {
+        data: { review, operation }
+      });
+  }
+  
+  deleteReview(reviewId: number) {
+    this._store.dispatch(updateActiveId({ activeId: reviewId }));
+    this._store.dispatch(resetReviewsStatus());
+    this._store.dispatch(deleteReview({ reviewId }));
+  }
 }
