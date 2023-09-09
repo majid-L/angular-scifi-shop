208c15e533f4b360df3c75b08c3d9a5c22f770ab
3782291211 <Laklouk98@hotmail.com>
2023-09-09 23:36:26 +0100

    Updating pagination logic

--- src/app/products/pagination/pagination.component.ts
+++ src/app/products/pagination/pagination.component.ts
@@ -1,14 +1,15 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatButtonToggleChange } from '@angular/material/button-toggle';
+import { ThemePalette } from '@angular/material/core';
 import { PageEvent } from '@angular/material/paginator';
 import { MatRadioChange } from '@angular/material/radio';
 import { MatSelectChange } from '@angular/material/select';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
 import { loadProducts } from 'src/app/ngrx/products/products.actions';
-import { selectLoadStatus, selectPagination } from 'src/app/ngrx/products/products.feature';
+import { selectLoadStatus, selectPagination, selectProducts } from 'src/app/ngrx/products/products.feature';
 
 @Component({
   selector: 'app-pagination',
@@ -21,7 +22,9 @@ export class PaginationComponent {
   readonly pagination$: Observable<Pagination> = 
     this._store.select(selectPagination);
   readonly productsLoadStatus$: Observable<Status> = 
-    this._store.select(selectLoadStatus)
+    this._store.select(selectLoadStatus);
+  readonly products$: Observable<Product[] | null> = 
+    this._store.select(selectProducts);
   private _paginationSubscription = Subscription.EMPTY;
   private _routeSubscription = Subscription.EMPTY;
   public currentPage = 1;
@@ -31,52 +34,98 @@ export class PaginationComponent {
   public maxPrice = 1e5;
   public category: string | undefined;
   public supplier: string | undefined;
-  private _order = "ASC";
-  private _hideOutOfStock = false;
+  public queryParams: ProductsUrlParams = {} as ProductsUrlParams;
+  public activeFilters: ProductsUrlParams = {} as ProductsUrlParams;
 
   constructor(
     private _store: Store<AppState>,
-    private _route: ActivatedRoute
+    private _route: ActivatedRoute,
+    private _router: Router
   ) { }
 
   ngOnInit() {
     this._paginationSubscription = this.pagination$.subscribe(({ page }) => {
       this.currentPage = page;
-    })
+    });
 
     this._routeSubscription = this._route.queryParamMap.subscribe(queryParamMap => {
+      this.queryParams = Object.create(queryParamMap).params;
+      this.activeFilters = this.selectFilters(Object.create(queryParamMap).params);
       this.category = queryParamMap.get("category") || "";
       this.supplier = queryParamMap.get("supplier") || "";
       this._store.dispatch(loadProducts(this.queryParams));
     });
   }
 
-  get queryParams(): ProductsUrlParams {
-    return {
-      page: this.currentPage,
-      limit: this.currentLimit,
-      orderBy: this.orderBy,
-      order: this._order,
-      minPrice: this.minPrice,
-      maxPrice: this.maxPrice,
-      hideOutOfStock: this._hideOutOfStock,
-      category: this.category,
-      supplier: this.supplier
+  get showFilters() {
+    return Object.keys(this.activeFilters).length !== 0;
+  }
+
+  get accentColor(): ThemePalette {
+    return document.body.classList.contains("light-mode") ? "primary" : "accent";
+  }
+
+  selectFilters({ page, limit, ...props}: ProductsUrlParams) {
+    const params: ProductsUrlParams = { ...props };
+    if (page && Number(page) > 1) params.page = page;
+    if (limit && Number(limit) !== 25) params.limit = limit;
+    return params;
+  }
+
+  removeFilter(param: string) {
+    this.updateUrlQueryParams({ [param]: null })
+  }
+
+  clearAllFilters() {
+    this._router.navigate([]);
+  }
+
+  updateUrlQueryParams(queryParams: { [key: string]: string | number | boolean | null }) {
+    this._router.navigate([], {
+      queryParams,
+      queryParamsHandling: "merge"
+    });
+  }
+
+  formatParamKey(key: string): string {
+    switch (key) {
+      case "orderBy":
+        return "Order by"
+      case "hideOutOfStock":
+        return "Hide out of stock";
+      case "minPrice":
+        return "Min price:";
+      case "maxPrice":
+        return "Max price:";
+      default:
+        return `${key[0].toUpperCase()}${key.slice(1)}:`;
     }
   }
 
-  formatLabel(value: number) {
-    return `£${value.toFixed(2)}`;
+  formatParamValue(value: string | number | boolean | undefined) {
+    switch (value) {
+      case "categoryName":
+        return "category";
+      case "supplierName":
+        return "supplier";
+      case "avgRating":
+        return "avg. rating";
+      case "ASC":
+        return "ascending";
+      case "DESC":
+        return "descending";
+      default:
+        return value;
+    }
   }
 
   handlePageEvent(e: PageEvent) {
-    const page = e.pageIndex + 1;
-    if (page !== this.currentPage || e.pageSize !== this.currentLimit) {
-      if (e.pageSize !== this.currentLimit) {
-        this.currentLimit = e.pageSize;
-      }
-      this._store.dispatch(loadProducts({ ...this.queryParams, page }));
-    }
+    this.currentPage = e.pageIndex + 1;
+    this.currentLimit = e.pageSize;
+    this.updateUrlQueryParams({ 
+      page: this.currentPage, 
+      limit: this.currentLimit 
+    });
   }
 
   emitStyleEvent(e: MatButtonToggleChange) {
@@ -84,46 +133,24 @@ export class PaginationComponent {
   }
 
   setOrderBy(e: MatSelectChange) {
-    if (!e.value) return;
-    this.orderBy = e.value;
+    const newParams: { orderBy: string, order?: string } = { orderBy: e.value };
     if (e.value === "bestsellers") {
-      this._order = "DESC";
+     newParams.order = "DESC";
     }
-    this._store.dispatch(loadProducts({ ...this.queryParams, orderBy: e.value }));
+    this.updateUrlQueryParams(newParams);
   }
 
   setOrder(e: MatRadioChange) {
-    this._order = e.value;
-    this._store.dispatch(loadProducts({ ...this.queryParams, order: e.value }));
+    this.updateUrlQueryParams({ order: e.value });
   }
 
   setHideOutOfStock(e: MatSlideToggleChange) {
-    this._hideOutOfStock = e.checked;
-    this._store.dispatch(loadProducts({ ...this.queryParams, hideOutOfStock: e.checked }));
+    this.updateUrlQueryParams({ hideOutOfStock: e.checked });
   }
 
   setPriceRange() {
-    console.log(this.minPrice, this.maxPrice);
-    this._store.dispatch(loadProducts(this.queryParams));
+    this.updateUrlQueryParams({ minPrice: this.minPrice, maxPrice: this.maxPrice });
   }
-
-  removeFilter() {
-    
-  }
-
-  /*setMinPrice(value: number) {
-    if (value !== this.minPrice) {
-      this.minPrice = value;
-      this._store.dispatch(loadProducts({ ...this.queryParams, minPrice: value }));
-    }
-  }
-
-  setMaxPrice(value: number) {
-    if (value !== this.maxPrice) {
-      this.maxPrice = value;
-      this._store.dispatch(loadProducts({ ...this.queryParams, maxPrice: value }));
-    }
-  }*/
 
   ngOnDestroy() {
     this._paginationSubscription.unsubscribe();
