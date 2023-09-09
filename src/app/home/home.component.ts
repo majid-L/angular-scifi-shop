bca100033317a9c6c1b1aa374bd5dd80b0f03342
3782291211 <Laklouk98@hotmail.com>
2023-09-09 23:34:08 +0100

    Updating styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { selectCategories, selectSuppliers } from '../ngrx/categories/categories.feature';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent {
+  suppliers$: Observable<Supplier[] | null> = this._store.select(selectSuppliers);
+  categories$: Observable<Category[] | null> = this._store.select(selectCategories);
+
+  constructor (
+    private _store: Store<AppState>
+  ) {}
+
+  get lightModeEnabled() {
+    return document.body.classList.contains("light-mode");
+  }
 }
