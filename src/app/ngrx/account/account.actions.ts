71a8435ec11a6277f8e79b0324717d037c53d52c
3782291211 <Laklouk98@hotmail.com>
2023-09-10 23:38:24 +0100

    Adding actions to account slice

--- src/app/ngrx/account/account.actions.ts
+++ src/app/ngrx/account/account.actions.ts
@@ -1,10 +1,57 @@
 import { createAction, props } from '@ngrx/store';
 
-export const loadAccount = createAction("[Account Component] Load Account Data");
-
+export const loadAccount = createAction(
+  "[Account Component] Load Account Data",
+  props<{ customerId: number }>()
+);
 export const loadAccountSuccess = createAction(
-  "[Account Response] Account Data Loaded - Success",
+  "[Account Component] Account Data Loaded - Success",
   props<Customer>()
+);
+
+export const updateAccount = createAction(
+  "[Account Component] Update Account - Loading",
+  props<{
+    requestBody: UpdateCustomerRequest,
+    customerId: number
+  }>()
+);
+export const updateAccountSuccess = createAction(
+  "[Account Component] Update Account - Success",
+  props<Customer>()
+);
+
+export const createOrUpdateAddress = createAction(
+  "[Account Component] Create Or Update Address - Loading",
+  props<{
+    requestBody: { billingAddress: Address } | { shippingAddress: Address },
+    customerId: number
+  }>()
+);
+export const createOrUpdateAddressSuccess = createAction(
+  "[Account Component] Create Or Update Address - Success",
+  props<Customer | CustomerNewAddress>()
+);
+
+export const deleteAddress = createAction(
+  "[Account Component] Delete Address - Loading",
+  props<{ 
+    addressId: number, 
+    customerId: number 
+  }>()
+);
+export const deleteAddressSuccess = createAction(
+  "[Account Component] Delete Address - Success",
+  props<Customer | { deletedAddress: Address }>()
+);
+
+export const deleteUser = createAction(
+  "[Account Component] Delete User - Loading",
+  props<{ customerId: number }>()
+);
+export const deleteUserSuccess = createAction(
+  "[Account Component] Delete User - Success",
+  props<DeleteUserResponse>()
 );
 
 export const clearCurrentUser = createAction("[Login Component] Clear Current User");
