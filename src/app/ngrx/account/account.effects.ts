fdd77461fc10b603dc0b0c9c22dce21b54c48999
3782291211 <Laklouk98@hotmail.com>
2023-09-10 23:37:25 +0100

    Adding NGRX effects to account slice

--- src/app/ngrx/account/account.effects.ts
+++ src/app/ngrx/account/account.effects.ts
@@ -1,26 +1,91 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { of } from 'rxjs';
 import { map, exhaustMap, catchError } from 'rxjs/operators';
 import { AccountService } from "src/app/account/account.service";
-import { loadAccount, loadAccountSuccess } from "./account.actions";
-import { httpError } from "../notification/notification.actions";
+import { createOrUpdateAddress, createOrUpdateAddressSuccess, deleteAddress, deleteAddressSuccess, deleteUser, deleteUserSuccess, loadAccount, loadAccountSuccess, updateAccount, updateAccountSuccess } from "./account.actions";
+import { dispatchErrorAction } from "..";
+import { notify } from "../notification/notification.actions";
+import { Router } from "@angular/router";
 
 @Injectable()
 export class AccountEffects {
-  loadAccount$ = createEffect(() => this.actions$.pipe(
+  loadAccount$ = createEffect(() => this._actions$.pipe(
     ofType(loadAccount),
-    exhaustMap(() => this.accountService.getAccountData()
+    exhaustMap(({ customerId }) => this._accountService.getAccountData(customerId)
     .pipe(
       map(accountResponse => {
         return loadAccountSuccess(accountResponse);
       }),
-      catchError(({ error }: { error: ApiError }) => of(httpError(error)))
+      catchError(dispatchErrorAction)
     ))
   ));
 
+  updateAccount$ = createEffect(() => this._actions$.pipe(
+    ofType(updateAccount),
+    exhaustMap(({ requestBody, customerId }) => {
+      return this._accountService.updateAccount(requestBody, customerId)
+      .pipe(
+        map(accountResponse => {
+          return updateAccountSuccess(accountResponse);
+        }),
+        catchError(dispatchErrorAction)
+      )
+    })
+  ));
+
+  createOrUpdateAddress$ = createEffect(() => this._actions$.pipe(
+    ofType(createOrUpdateAddress),
+    exhaustMap(({ requestBody, customerId }) => {
+      return this._accountService.createOrUpdateAddress(requestBody, customerId)
+      .pipe(
+        map(response => {
+          return createOrUpdateAddressSuccess(response);
+        }),
+        catchError(dispatchErrorAction)
+      )
+    })
+  ));
+
+  deleteAddress$ = createEffect(() => this._actions$.pipe(
+    ofType(deleteAddress),
+    exhaustMap(({ addressId, customerId }) => {
+      return this._accountService.deleteAddress(addressId, customerId)
+      .pipe(
+        map(response => deleteAddressSuccess(response)),
+        catchError(dispatchErrorAction)
+      )
+    })
+  ));
+
+  deleteUser$ = createEffect(() => this._actions$.pipe(
+    ofType(deleteUser),
+    exhaustMap(({ customerId }) => {
+      return this._accountService.deleteAccount(customerId)
+      .pipe(
+        map(deleteUserResponse => {
+          return deleteUserSuccess(deleteUserResponse);
+        }),
+        catchError(dispatchErrorAction)
+      )
+    })
+  ));
+
+  deleteUserNotification$ = createEffect(() => this._actions$.pipe(
+    ofType(deleteUserSuccess),
+    map(payload => {
+      window.localStorage.removeItem('userId');
+      this._router.navigate(['/']);
+      return notify({ 
+        title: "You have successfully deleted your account.", 
+        content: payload.msg,
+        deletedUser: payload.deletedUser
+      });
+    })
+  ));
+
   constructor(
-    private actions$: Actions,
-    private accountService: AccountService
+    private _actions$: Actions,
+    private _router: Router,
+    private _accountService: AccountService
   ) {}
 }
