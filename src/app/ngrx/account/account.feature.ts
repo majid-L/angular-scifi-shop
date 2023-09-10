9d5e8ee8b335d80118acdaba3cc611cab080494b
3782291211 <Laklouk98@hotmail.com>
2023-09-10 23:38:43 +0100

    Adding actions to account reducer

--- src/app/ngrx/account/account.feature.ts
+++ src/app/ngrx/account/account.feature.ts
@@ -1,16 +1,92 @@
 import { createFeature, createReducer, createSelector, on } from '@ngrx/store';
 import { httpError } from '../notification/notification.actions';
-import { clearCurrentUser, loadAccount, loadAccountSuccess } from './account.actions';
+import { clearCurrentUser, createOrUpdateAddress, createOrUpdateAddressSuccess, deleteAddress, deleteAddressSuccess, deleteUser, deleteUserSuccess, loadAccount, loadAccountSuccess, updateAccount, updateAccountSuccess } from './account.actions';
 
 const initialState: AccountState = {
   account: null,
-  status: "pending"
+  loadStatus: "pending",
+  updateStatus: "pending",
+  deleteStatus: "pending"
 };
+
+const removeAddressFromAccount = (account: Customer, deletedAddressId: number) => {
+  const accountCopy: Customer = { 
+    ...account, 
+    billingAddress: account.billingAddress ? { ...account.billingAddress } : null,
+    shippingAddress: account.shippingAddress ? { ...account.shippingAddress } : null
+  };
+
+  if (accountCopy.billingAddress?.id === deletedAddressId) {
+    delete accountCopy.billingAddress;
+  }
+  if (accountCopy.shippingAddress?.id === deletedAddressId) {
+    delete accountCopy.shippingAddress;
+  }
+
+  return accountCopy;
+}
 
 export const accountReducer = createReducer(
   initialState,
-  on(loadAccount, state => ({ ...state, status: "loading" as const })),
-  on(loadAccountSuccess, (state, account) => ({ account, status: "success" as const})),
+  on(loadAccount, state => ({ 
+    ...state,
+    loadStatus: "loading" as const 
+  })),
+  on(loadAccountSuccess, (state, payload) => ({
+    ...state,
+    account: payload,
+    loadStatus: "success" as const
+  })),
+  on(updateAccount, createOrUpdateAddress, state => ({
+    ...state,
+    updateStatus: "loading" as const
+  })),
+  on(updateAccountSuccess, (state, payload) => ({
+    ...state,
+    account: payload,
+    updateStatus: "loading" as const
+  })),
+  on(createOrUpdateAddressSuccess, (state, payload) => {
+    if (payload.hasOwnProperty("newAddress")) {
+      return {
+        ...state,
+        account: (payload as CustomerNewAddress).customer,
+        updateStatus: "success" as const
+      }
+    } else {
+      return {
+        ...state,
+        account: payload as Customer,
+        updateStatus: "success" as const
+      }
+    }
+  }),
+  on(deleteAddress, deleteUser, state => ({
+    ...state,
+    deleteStatus: "loading" as const
+  })),
+  on(deleteAddressSuccess, (state, payload) => {
+    if (payload.hasOwnProperty("deletedAddress")) {
+      const deletedAddressId = (payload as { deletedAddress: Address }).deletedAddress.id!;
+      const updatedAccount = removeAddressFromAccount(state.account!, deletedAddressId);
+      return {
+        ...state,
+        account: updatedAccount,
+        deleteStatus: "success" as const
+      }
+    } else {
+      return {
+        ...state,
+        account: payload as Customer,
+        deleteStatus: "success" as const
+      }
+    }
+  }),
+  on(deleteUserSuccess, state => ({
+    ...state,
+    account: null,
+    deleteStatus: "success" as const
+  })),
   on(clearCurrentUser, state => ({ ...state, account: null })),
   on(httpError, state => ({ ...state, status: "error" as const }))
 );
@@ -32,7 +108,9 @@ export const accountFeature = createFeature({
 
 export const {
   selectAccount,
-  selectStatus,
+  selectLoadStatus,
+  selectUpdateStatus,
+  selectDeleteStatus,
   selectBillingAddress,
   selectShippingAddress
 } = accountFeature;
