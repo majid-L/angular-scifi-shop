4250b02cba92c49612fabeb37d652e037bc3bbb9
3782291211 <Laklouk98@hotmail.com>
2023-09-10 23:37:53 +0100

    Adding wishlist CRUD logic to products component

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,12 +1,22 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 import { MatButtonToggleChange } from '@angular/material/button-toggle';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { combineLatest, map, Observable, Subscription } from 'rxjs';
-import { selectActiveId, selectUpdateStatus } from 'src/app/ngrx/cart/cart.feature';
+import { 
+  selectActiveId as selectActiveCartId, 
+  selectUpdateStatus as selectCartUpdateStatus
+} from 'src/app/ngrx/cart/cart.feature';
+import { 
+  selectActiveId as selectActiveWishlistId, 
+  selectUpdateStatus as selectWishlistUpdateStatus
+} from 'src/app/ngrx/wishlist/wishlist.feature';
 import { selectCategories, selectSuppliers } from 'src/app/ngrx/categories/categories.feature';
 import { selectLoadStatus, selectProducts } from 'src/app/ngrx/products/products.feature';
+import { resetWishlistStatus, updateActiveId, updateWishlist } from 'src/app/ngrx/wishlist/wishlist.actions';
+import { selectWishlist } from 'src/app/ngrx/wishlist/wishlist.feature';
 
 @Component({
   selector: 'app-product-list',
@@ -14,28 +24,33 @@ import { selectLoadStatus, selectProducts } from 'src/app/ngrx/products/products
   styleUrls: ['./product-list.component.sass']
 })
 export class ProductListComponent implements OnInit {
+  wishlist$: Observable<Wishlist | null> = this._store.select(selectWishlist);
   products$: Observable<Product[] | null> = this._store.select(selectProducts);
   categories$: Observable<Category[] | null> = this._store.select(selectCategories);
   suppliers$: Observable<Supplier[] | null> = this._store.select(selectSuppliers);
   productLoadStatus$: Observable<Status> = this._store.select(selectLoadStatus);
-  cartUpdateStatus$: Observable<Status> = this._store.select(selectUpdateStatus);
-  activeId$: Observable<number> = this._store.select(selectActiveId);
+  cartUpdateStatus$: Observable<Status> = this._store.select(selectCartUpdateStatus);
+  wishlistUpdateStatus$: Observable<Status> = this._store.select(selectWishlistUpdateStatus)
+  cartActiveId$: Observable<number> = this._store.select(selectActiveCartId);
+  wishlistActiveId$: Observable<number> = this._store.select(selectActiveWishlistId);
   private _breakpointSubscription = Subscription.EMPTY;
   private _streamSubscription = Subscription.EMPTY;
   listDisplayStyle = "grid";
   showDisplayToggle = true;
+  private _wishlistOperation: "add" | "remove" | undefined;
   category: Category | null | undefined = null;
   supplier: Supplier | null | undefined = null;
   datastream$ = combineLatest([
-    this.categories$, this.suppliers$, this._route.queryParamMap
-  ]).pipe(map(([categories, suppliers, queryParamMap]) => ({
-    categories, suppliers, queryParamMap
+    this.categories$, this.suppliers$, this._route.queryParamMap, this.wishlistUpdateStatus$
+  ]).pipe(map(([categories, suppliers, queryParamMap, wishlistUpdateStatus]) => ({
+    categories, suppliers, queryParamMap, wishlistUpdateStatus
   })));
 
   constructor(
     private _store: Store<AppState>,
     private _route: ActivatedRoute,
-    private _breakPointObserver: BreakpointObserver
+    private _breakPointObserver: BreakpointObserver,
+    private _snackBar: MatSnackBar
   ) {}
 
   ngOnInit() {
@@ -51,7 +66,7 @@ export class ProductListComponent implements OnInit {
       });
 
     this._streamSubscription = this.datastream$
-      .subscribe(({ categories, suppliers, queryParamMap }) => {
+      .subscribe(({ categories, suppliers, queryParamMap, wishlistUpdateStatus }) => {
         const categoryParam = queryParamMap.get("category");
         const supplierParam = queryParamMap.get("supplier");
         if (categoryParam && categories) {
@@ -65,12 +80,22 @@ export class ProductListComponent implements OnInit {
         } else if (!supplierParam) {
           this.supplier = null;
         }
+
+        if (wishlistUpdateStatus === "success") {
+          const message = this._wishlistOperation === "add" ? "Product successfully added to wishlist." : "Product successfully removed from wishlist.";
+          this._snackBar.open(message, "Dismiss", {
+            horizontalPosition: "start",
+            verticalPosition: "top",
+            duration: 8000
+          });
+        }
       });
   }
 
   ngOnDestroy() {
     this._breakpointSubscription.unsubscribe();
     this._streamSubscription.unsubscribe();
+    this._store.dispatch(resetWishlistStatus());
   }
 
   get headerImageSrc() {
@@ -86,6 +111,34 @@ export class ProductListComponent implements OnInit {
       "product-list": this.listDisplayStyle === 'list',
       "product-grid": this.listDisplayStyle === 'grid'
     };
+  }
+
+  productIsInWishlist(productId: number, wishlist: Wishlist) {
+    return wishlist.wishlistItems.find(item => item.product.id === productId);
+  }
+
+  updateWishlist(productId: number, wishlist: Wishlist, operation: "add" | "remove") {
+    this._wishlistOperation = operation;
+    this._store.dispatch(updateActiveId({ activeId: productId }));
+    let wishlistArray: WishlistItem[] | [] = [];
+    const transformWishlistArray = (item: { product: Product }) => ({ 
+      customerId: wishlist.id,
+      productId: item.product.id
+    });
+
+    if (operation === "add") {
+      wishlistArray = wishlist.wishlistItems.map(transformWishlistArray);
+      wishlistArray.unshift({ customerId: wishlist.id, productId });
+    } else {
+      wishlistArray = wishlist.wishlistItems
+      .filter(item => item.product.id !== productId)
+      .map(transformWishlistArray);
+    }
+      
+    this._store.dispatch(updateWishlist({
+      updatedWishlist: wishlistArray,
+      customerId: wishlist.id
+    }));
   }
 
   toggleStyle(e: MatButtonToggleChange) {
