3cd3b4bc3fbd3e70903a49b93083817326a57826
3782291211 <Laklouk98@hotmail.com>
2023-09-12 07:14:24 +0100

    Adding CRUD operations to account page forms

--- src/app/account/account/account.component.ts
+++ src/app/account/account/account.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Store } from '@ngrx/store';
-import { Observable, Subscription } from 'rxjs';
+import { combineLatest, map, Observable, Subscription } from 'rxjs';
 import { loadAccount, resetStatus, updateAccount, updateActiveItem } from 'src/app/ngrx/account/account.actions';
 import { selectAccount, selectLoadStatus, selectUpdateStatus, selectDeleteStatus, selectActiveItem } from 'src/app/ngrx/account/account.feature';
 import { selectLoggedInUserId } from 'src/app/ngrx/auth/auth.feature';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-account',
@@ -18,11 +20,29 @@ export class AccountComponent implements OnInit {
     this._store.select(selectAccount);
   readonly accountLoadStatus$: Observable<Status> = 
     this._store.select(selectLoadStatus);
-  // account update status
-  // account active item
-  private _accountDataSubscription = Subscription.EMPTY;
-  private _loggedInUserIdSubscription = Subscription.EMPTY;
+  readonly updateStatus$: Observable<Status> = 
+    this._store.select(selectUpdateStatus);
+  readonly deleteStatus$: Observable<Status> = 
+    this._store.select(selectDeleteStatus);
+  readonly activeItem$: Observable<AccountActiveItem> = 
+    this._store.select(selectActiveItem);
+
+  readonly dataStream$ = combineLatest([
+    this.loggedInUserId$, this.accountData$, this.activeItem$
+  ]).pipe(map(([loggedInUserId, accountData, activeItem]) => ({
+    loggedInUserId, accountData, activeItem
+  })));
+
+  readonly statusStream$ = combineLatest([this.updateStatus$, this.deleteStatus$])
+  .pipe(map(([ updateStatus, deleteStatus]) => ({ updateStatus, deleteStatus })));
+  
+  private _subscription = Subscription.EMPTY;
+  private _statusSubscription = Subscription.EMPTY;
   private _loggedInUserId: number | undefined;
+  activeItem: AccountActiveItem = null;
+  updateStatus: Status = "pending";
+  deleteStatus: Status = "pending";
+
   accountForm = this._formBuilder.group({
     name: [""],
     username: [""],
@@ -39,27 +59,53 @@ export class AccountComponent implements OnInit {
 
   constructor(
     private _store: Store<AppState>,
-    private _formBuilder: FormBuilder
+    private _formBuilder: FormBuilder,
+    private _accountService: AccountService,
+    private _snackBar: MatSnackBar
   ) { }
 
   ngOnInit() {
-    this._loggedInUserIdSubscription = this.loggedInUserId$.subscribe(id => {
-      if (id) {
-        this._loggedInUserId = Number(id);
-        this._store.dispatch(loadAccount({ customerId: Number(id) }));
+    this._subscription = this.dataStream$.subscribe(({
+      loggedInUserId, accountData, activeItem
+    }) => {
+      if (loggedInUserId) {
+        this._loggedInUserId = Number(loggedInUserId);
+        //this._store.dispatch(loadAccount({ customerId: Number(loggedInUserId) }));
       }
-    });
-    this._accountDataSubscription = this.accountData$.subscribe(data => {
-      if (data) {
+
+      if (accountData) {
         this.accountForm = this._formBuilder.group({
-          name: [data.name, Validators.pattern(/[\S]+/)],
-          username: [data.username, Validators.pattern(/[\S]+/)],
-          email: [data.email, Validators.email],
-          phone: [data.phone, [Validators.pattern(/^[^a-zA-Z]+$/), Validators.pattern(/[\S]+/)]],
-          avatar: [data.avatar, Validators.pattern(/[\S]+/)]
+          name: [accountData.name, Validators.pattern(/[\S]+/)],
+          username: [accountData.username, Validators.pattern(/[\S]+/)],
+          email: [accountData.email, Validators.email],
+          phone: [accountData.phone, [Validators.pattern(/^[^a-zA-Z]+$/), Validators.pattern(/[\S]+/)]],
+          avatar: [accountData.avatar, Validators.pattern(/[\S]+/)]
         });
       }
+
+      this.activeItem = activeItem;
     });
+
+    this._statusSubscription = this.statusStream$
+      .subscribe(({ updateStatus, deleteStatus }) => {
+        this.updateStatus = updateStatus;
+        this.deleteStatus = deleteStatus;
+
+        if (updateStatus === "success" || deleteStatus === "success") {
+          let message = 
+            this.activeItem === "billingAddress" ? "Billing address " 
+            : this.activeItem === "shippingAddress" ? "Shipping address"
+            : this.activeItem === "password" ? "Password "
+            : "Account ";
+          if (updateStatus === "success") message += "updated successfully.";
+          if (deleteStatus === "success") message += "deleted successfully.";
+          this._snackBar.open(message, "Dismiss", {
+            horizontalPosition: "start",
+            verticalPosition: "top",
+            duration: 8000
+          });
+        }
+      });
   }
 
   get name() { return this.accountForm.get("email"); }
@@ -89,20 +135,29 @@ export class AccountComponent implements OnInit {
   }
 
   updateAccountData() {
+    const requestBody = this._accountService
+    .removeEmptyFields<UpdateCustomerRequest>(
+      this.accountForm.value as UpdateCustomerRequest
+    );
     this._store.dispatch(resetStatus());
     this._store.dispatch(updateActiveItem({ activeItem: "account" }));
     this._store.dispatch(updateAccount({
-      requestBody: this.accountForm.value as UpdateCustomerRequest,
+      requestBody,
       customerId: this._loggedInUserId!
     }));
   }
 
   updatePassword() {
-
+    this._store.dispatch(resetStatus());
+    this._store.dispatch(updateActiveItem({ activeItem: "password" }));
+    this._store.dispatch(updateAccount({
+      requestBody:this.passwordForm.value as UpdateCustomerRequest,
+      customerId: this._loggedInUserId!
+    }));
   }
 
   ngOnDestroy() {
-    this._accountDataSubscription.unsubscribe();
-    this._loggedInUserIdSubscription.unsubscribe();
+    this._subscription.unsubscribe();
+    this._statusSubscription.unsubscribe();
   }
 }
