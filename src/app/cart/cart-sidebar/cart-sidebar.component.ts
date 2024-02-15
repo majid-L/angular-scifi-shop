a37e547efc608634ada41bf784fd29e163fd4ceb
majid-L <Laklouk98@hotmail.com>
2024-02-15 12:42:48 +0000

    Updating various styles

--- src/app/cart/cart-sidebar/cart-sidebar.component.ts
+++ src/app/cart/cart-sidebar/cart-sidebar.component.ts
@@ -5,13 +5,13 @@ import { Observable, Subscription, EMPTY } from 'rxjs';
 @Component({
   selector: 'app-cart-sidebar',
   templateUrl: './cart-sidebar.component.html',
-  styleUrls: ['./cart-sidebar.component.sass']
+  styleUrls: ['./cart-sidebar.component.sass'],
 })
 export class CartSidebarComponent implements AfterViewInit {
   @Input() events: Observable<void> = EMPTY;
   @ViewChild(MatSidenav) cart: MatSidenav | undefined;
   private _eventsSubscription: Subscription = Subscription.EMPTY;
- 
+
   ngAfterViewInit() {
     this._eventsSubscription = this.events.subscribe(() => {
       this.cart?.open();
@@ -20,5 +20,13 @@ export class CartSidebarComponent implements AfterViewInit {
 
   ngOnDestroy() {
     this._eventsSubscription.unsubscribe();
+  }
+
+  toggleBodyOverflow(state: "hide" | "show") {
+    if (state === "hide") {
+      document.body.classList.add("hide-scrollbar");
+    } else {
+      document.body.classList.remove("hide-scrollbar");
+    }
   }
 }
